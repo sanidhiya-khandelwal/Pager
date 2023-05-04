09832899ad550632e49917ce1ecfb8d544f6a22c
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-05-04 19:24:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'React'
+import Form from './components/Form'
+import './App.css'
+function App() {
+    return (
+        <div className="app">
+            <div className="container">
+                <div className="shape-1"></div>
+                <div className="shape-2"></div>
+                <Form />
+            </div>
+        </div>
+    )
+}
+export default App
